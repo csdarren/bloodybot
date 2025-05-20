bb10a5d8e36a7fc140d078c776d17c5062afd122
csdarren <2001dhall@gmail.com>
2025-05-20 18:48:04 -0400

    fixed channel edit for a 3rd time

--- exts/create_channel.py
+++ exts/create_channel.py
@@ -10,15 +10,14 @@ from bot import BloodyBot
 class CreateChannelCog(commands.Cog):
     def __init__(self, bot: BloodyBot):
         self.bot = bot
-        self.created_channels: set[VoiceChannel] = set()
-        self.members_created_channels: set [Member] = set()
+        self.created_channel_data = []
         self.update_channel_names.start()
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member: Member, before: VoiceState, after: VoiceState) -> None:
         print(f"member: {member.display_name} joined {after.channel} from {before.channel}")
 
-        for channel in self.created_channels:
+        for channel, temp_member in self.created_channel_data:
             if isinstance(channel, VoiceChannel) and not channel.members:
                 await channel.delete()
 
@@ -28,8 +27,7 @@ class CreateChannelCog(commands.Cog):
                 # Create channel in target_category.
                 created_channel = await target_category.create_voice_channel(f"{member.display_name} smelt it")
                 # Adds the channel to a list that is stored in the bot class for later use in the tasks.
-                self.created_channels.add(created_channel)
-                self.members_created_channels.add(member)
+                self.created_channel_data.append([created_channel, member])
                 print(f"Moving {member.name} to {created_channel.name}")
                 # Moves the member that caused the voice state update to the created channel
                 await member.move_to(created_channel)
@@ -37,15 +35,13 @@ class CreateChannelCog(commands.Cog):
     @tasks.loop(minutes=10)
     async def update_channel_names(self) -> None:
         print("Loop started ----------------------------------------------------------------------------")
-        for channel in self.created_channels:
+        for channel, member in self.created_channel_data:
             if isinstance(channel, VoiceChannel) and channel.name != "Create Channel":
                 creation_time = channel.created_at
                 current_time = datetime.now(tz=UTC)
                 time_passed = current_time - creation_time
                 hours_passed = int(time_passed.total_seconds() / 3600)
-                for member in self.members_created_channels:
-                    if member.display_name in channel.name:
-                        await channel.edit(name=f"{member.display_name} smelt it {hours_passed} hours ago")
+                await channel.edit(name=f"{member.display_name} smelt it {hours_passed} hours ago")
         print("Loop ended ------------------------------------------------------------------------------")
 
 
