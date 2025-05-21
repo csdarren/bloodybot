4bf60fef16e2ddf51848d7af519f6bfb73f196a3
csdarren <2001dhall@gmail.com>
2025-05-20 20:00:48 -0400

    fixed channel edit for 6th time

--- exts/create_channel.py
+++ exts/create_channel.py
@@ -1,4 +1,3 @@
-
 from datetime import UTC, datetime
 
 from discord import Member, VoiceChannel, VoiceState
@@ -13,43 +12,52 @@ class CreateChannelCog(commands.Cog):
         self.created_channel_data = []
         self.update_channel_names.start()
 
+
     @commands.Cog.listener()
     async def on_voice_state_update(self, member: Member, before: VoiceState, after: VoiceState) -> None:
         print(f"member: {member.display_name} joined {after.channel} from {before.channel}")
 
+
         for channel, temp_member in self.created_channel_data:
-            if isinstance(channel, VoiceChannel) and not channel.members:
-                self.created_channel_data.remove([channel, temp_member])
-                await channel.delete()
+            if not isinstance(channel, VoiceChannel) and not channel.members:
+                return
+            self.created_channel_data.remove([channel, temp_member])
+            await channel.delete()
+
 
         if after.channel and after.channel.name == "Create Channel":
             target_category = after.channel.category
-            if target_category:
-                # Create channel in target_category.
-                created_channel = await target_category.create_voice_channel(f"{member.display_name} smelt it")
-                # Adds the channel to a list that is stored in the bot class for later use in the tasks.
-                self.created_channel_data.append([created_channel, member])
-                print(f"Moving {member.name} to {created_channel.name}")
-                # Moves the member that caused the voice state update to the created channel
-                await member.move_to(created_channel)
+            if not target_category:
+                return
+            # Create channel in target_category.
+            created_channel = await target_category.create_voice_channel(f"{member.display_name} smelt it")
+            # Adds the channel to a list that is stored in the bot class for later use in the tasks.
+            self.created_channel_data.append([created_channel, member])
+            print(f"Moving {member.name} to {created_channel.name}")
+            # Moves the member that caused the voice state update to the created channel
+            await member.move_to(created_channel)
+
 
     @tasks.loop(minutes=10)
     async def update_channel_names(self) -> None:
-        print("Loop started ----------------------------------------------------------------------------")
         for channel, member in self.created_channel_data:
-            if isinstance(channel, VoiceChannel) and channel.name != "Create Channel":
-                creation_time = channel.created_at
-                current_time = datetime.now(tz=UTC)
-                time_passed = current_time - creation_time
-                hours_passed = int(time_passed.total_seconds() / 3600)
-                if not hours_passed > 1:
-                    channel_edit_str = f"{member.display_name} smelt it {hours_passed} hours ago"
-                    if channel.name != channel_edit_str:
-                        await channel.edit(name=channel_edit_str)
+            if not isinstance(channel, VoiceChannel):
+                continue
+            if channel.name == "Create Channel":
+                continue
+
+            creation_time = channel.created_at
+            current_time = datetime.now(tz=UTC)
+            time_passed = current_time - creation_time
+            hours_passed = int(time_passed.total_seconds() / 3600)
+
+            if hours_passed < 1:
                 channel_edit_str = f"{member.display_name} smelt it recently"
-                if channel.name != channel_edit_str:
-                    await channel.edit(name=channel_edit_str)
-        print("Loop ended ------------------------------------------------------------------------------")
+            else:
+                channel_edit_str = f"{member.display_name} smelt it {hours_passed} hours ago"
+
+            if channel.name != channel_edit_str:
+                await channel.edit(name=channel_edit_str)
 
 
 async def setup(bot: BloodyBot) -> None:
